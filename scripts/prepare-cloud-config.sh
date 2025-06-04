e46b16f640b38d261c6e095ad6d751eef5c53faf
David Hoppe <dh@smpmedia.net>
2025-06-04 11:00:05 +0200

    Add multiple keys into cloud config

--- scripts/prepare-cloud-config.sh
+++ scripts/prepare-cloud-config.sh
@@ -13,10 +13,16 @@ fi
 # Create output directory
 mkdir -p output
 
+IFS=',' read -ra KEYS <<< "$SSH_KEYS"
+SSH_YAML="\n"
+for key in "${KEYS[@]}"; do
+  SSH_YAML+="      - ${key}\n"
+done
+
 echo "🔧 Generating customized cloud-config.yaml..."
 sed -e "s|__DOMAINS__|$DOMAINS|g" \
     -e "s|__DATA_VOLUME_PATH__|$DATA_VOLUME_PATH|g" \
-    -e "s|__SSH_KEY__|$SSH_KEY|g" \
+    -e "s|__SSH_KEYS__|${SSH_YAML}|g" \
     -e "s|__LABEL__|$LABEL|g" \
     templates/cloud-config.template.yaml > output/cloud-config.yaml
 
